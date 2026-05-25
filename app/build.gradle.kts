d96719d2a46bdf03ba0843ddf97a673e94a417ef
tung2882005-hub <tung2882005@gmail.com>
2026-05-25 17:34:59 +0700

    thêm trang don dat hang

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,9 @@ dependencies {
 
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
+    // Material Design — cần cho TabLayout, BottomNavigationView
+    implementation("com.google.android.material:material:1.11.0")
+// ViewPager2
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
 }
